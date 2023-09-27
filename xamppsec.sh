e4add7dd9a33ffc44b7d7ade305f090dfb526fab
astra <26215963+3tternp@users.noreply.github.com>
2023-09-27 23:32:35 +0545

    Update xamppsec.sh

--- xamppsec.sh
+++ xamppsec.sh
@@ -1,2 +1,9 @@
-#! /bin/bash
+#!/bin/bash
+
+# This script starts the security features of the LAMP server.
+
+# Start the security features.
 sudo /opt/lampp/lampp security
+
+# Exit the script.
+exit 0
